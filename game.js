fe0b442579326aef396e2237ffb2d1382a4bdce0
John <johnsief12@gmail.com>
2023-01-21 12:29:35 -0500

    added click handler function

--- game.js
+++ game.js
@@ -1,5 +1,14 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
+let userClickedPattern = [];
+
+
+
+$(".btn").on("click", function(){
+    const userChosenColor = $(this).attr("id")
+    userClickedPattern.push(userChosenColor);
+    console.log(userClickedPattern)
+    })
 
 
 
@@ -9,12 +18,11 @@ const randomNumber = Math.floor(Math.random() * 4);
 const randomChosenColor = buttonColors[randomNumber];
 gamePattern.push(randomChosenColor)
 
-
-
 $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
 
 let audio = new Audio("sounds/" + randomChosenColor + ".mp3");
 audio.play();
+
 
 
 }
