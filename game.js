de594b14bce1d0129c247117e85b86ad957e90d8
John <johnsief12@gmail.com>
2023-01-21 11:35:08 -0500

    added sound and keypress fade in/out on colors

--- game.js
+++ game.js
@@ -4,10 +4,24 @@ let gamePattern = [];
 
 
 const nextSequence = () => {
+
 const randomNumber = Math.floor(Math.random() * 4);
 const randomChosenColor = buttonColors[randomNumber];
 gamePattern.push(randomChosenColor)
 
 
+
+$("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
+let audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+audio.play();
+
+
 }
-nextSequence()
+
+
+$(document).keypress(function() {
+
+    nextSequence();
+
+});
