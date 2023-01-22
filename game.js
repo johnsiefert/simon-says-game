46a061f05d8d7fc9792f397efcb13b66a30aadd2
John <johnsief12@gmail.com>
2023-01-22 13:23:07 -0500

    press key to start game

--- game.js
+++ game.js
@@ -1,25 +1,46 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
+let started = false;
+let level = 0
 
+$(document).keypress(function() {
+    if (!started) {
+      $("#level-title").text("Level " + level);
+    nextSequence();
+      started = true;
+    }
+  });
 
-
+//clicks colors by id
 $(".btn").on("click", function(){
     const userChosenColor = $(this).attr("id")
     userClickedPattern.push(userChosenColor);
+
     playSound(userChosenColor);
+    animatePress(userChosenColor);
+    
     })
 
+    // $(document).keypress(function (event) {
+    //     if(String.fromCharCode(event.which)==='a'){
+    //         console.log(nextSequence())
+    //     }
+    // })
 
-
+//keypress random colors
 const nextSequence = () => {
+
+    level++;
+$("#level-title").text("Level " + level);
 
 const randomNumber = Math.floor(Math.random() * 4);
 const randomChosenColor = buttonColors[randomNumber];
 gamePattern.push(randomChosenColor)
 
 $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
-playSound(randomChosenColor)
+playSound(randomChosenColor);
+
 }
 
 const playSound = (name) => {
@@ -28,11 +49,12 @@ audio.play();
 }
 
 
+//adds flash animation when press/clicked
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function () {
+      $("#" + currentColor).removeClass("pressed");
+    }, 100);
+  }
 
 
-
-$(document).keypress(function() {
-
-    nextSequence();
-
-});
